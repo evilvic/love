59dc8fc0b427a974be3cdf8310882099e5d90759
Víctor Peña Romero <vic.pero@icloud.com>
2020-01-20 14:15:42 -0600

    add class Key

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
-// canvas
+/* ----- CANVAS ----- */
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-// images
+/* ----- IMAGES ----- */
 const images = {
     blTile: './assets/images/tile-blue.png',
     orTile: './assets/images/tile-orange.png',
     piTile: './assets/images/tile-pink.png',
     spTile: './assets/images/tile-gray.png',
-    grTile: './assets/images/tile-green.png'
+    grTile: './assets/images/tile-green.png',
+    token: './assets/images/token.png',
+    boy: './assets/images/ch-boy.png'
 }
 
-// levels
+/* ----- LEVELS ----- */
 const level1 = {
     width: 3,
     height: 3,
     map: [
-        ['B', 'B', 'P'],
-        ['O', '0', 'O'],
+        ['B', 'B', 'B'],
+        ['O', 'O', 'O'],
         ['B', 'B', 'B']
     ],
-    exit: [4, 2]
+    exit: [4, 2],
+    start: [1, 2],
+    key:  [2, 2]
+
 }
 
 const level2 = {
     width: 4,
-    height: 4
+    height: 4,
+    map: [
+        ['O', 'B', 'B', 'P'],
+        ['B', 'P', 'P', 'B'],
+        ['B', 'P', 'P', 'B'],
+        ['B', 'B', 'B', 'O']
+    ],
+    exit: [4, 5],
+    start: [4, 4],
+    key: [1, 1]
 }
 
-// maps
+/* ----- CLASSES ----- */
 class Map {
-    constructor(obj) {
+    constructor(x, obj) {
+        this.x = x
         this.width = obj.width
         this.height = obj.height
         this.map = obj.map
         this.exit = obj.exit
+        this.key = obj.key
     }
     draw() {
         this.map.forEach((array, i) => {
@@ -43,25 +59,29 @@ class Map {
                     case '0':
                         break
                     case 'B':
-                        new Tile(100 + j * 55, 100 + i * 55, images.blTile)
+                        new Tile(this.x + j * 55, 200 + i * 55, images.blTile)
                         break
                     case 'O':
-                        new Tile(100 + j * 55, 100 + i * 55, images.orTile)
+                        new Tile(this.x + j * 55, 200 + i * 55, images.orTile)
                         break
                     case 'P':
-                        new Tile(100 + j * 55, 100 + i * 55, images.piTile)
+                        new Tile(this.x + j * 55, 200 + i * 55, images.piTile)
                 }
             })
         })
         new Tile(
-                100 + (this.exit[0] - 1) * 55, 
-                100 + (this.exit[1] - 1) * 55, 
+                this.x + (this.exit[0] - 1) * 55, 
+                200 + (this.exit[1] - 1) * 55, 
                 images.spTile
+                )
+        new Key(
+                this.x + 15 + (this.key[0] - 1) * 55, 
+                200 + 13 + (this.key[1] - 1) * 55, 
+                images.token
                 )
     }
 }
 
-// tiles
 class Tile {
     constructor(x, y, src) {
         this.x = x
@@ -79,6 +99,25 @@ class Tile {
     }
 }
 
+class Key {
+    constructor(x, y, src) {
+        this.x = x
+        this.y = y
+        this.width = 20
+        this.height = 20
+        this.img = new Image()
+        this.img.src = src
+        this.img.onload = () => {
+            this.draw()
+        }
+    }
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+}
+
+/* ----- RENDER ----- */
 window.onload = () => {
-    new Map(level1).draw()
+    new Map(100, level1).draw()
+    new Map(550, level2).draw()
 }
